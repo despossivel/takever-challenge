6fc5d1c48471597e2ae89c1b55666d22b84d504c
Matheus Henrique Sousa Brito <despossivel@despossivel.local>
2022-10-14 16:10:48 -0300

    /env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const app = require('./src/server'),
-    PORT = 5005;
+    PORT = process.env.PORT || 80;
 
-app.listen(process.env.PORT || PORT, _ => console.log(`SERVER ON-LINE NA PORTA ${PORT}`));
+app.listen(PORT, _ => console.log(`SERVER ON-LINE NA PORTA ${PORT}`));
