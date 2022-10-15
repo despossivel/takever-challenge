ad83ac945393ba86f5991bceae9c1b2f654b0ec9
Matheus Henrique Sousa Brito <despossivel@despossivel.local>
2022-10-14 23:56:07 -0300

    hotfix: require not load

--- src/routes/favoriteTvShow.js
+++ src/routes/favoriteTvShow.js
@@ -1,12 +1,12 @@
  const { Router } = require('express'),
     route = new Router(),
     middleware = require('../middlewares/routes/favoriteTvShow'),
-    TvShow = require('../controllers/favoriteTvShow');
+    FavoriteTvShow = require('../controllers/FavoriteTvShow');
 
-route.post('/favorite/tv/show/create', middleware.create, TvShow.store)
-route.get('/favorite/tv/show/get', middleware.index, TvShow.index)
-route.get('/favorite/tv/show/:user_id', middleware.show, TvShow.show)
-// route.put('/favorite/tv/show/:_id', middleware.update, TvShow.update)
-route.delete('/favorite/tv/show/:_id', middleware.destroy, TvShow.destroy)
+route.post('/favorite/tv/show/create', middleware.create, FavoriteTvShow.store)
+route.get('/favorite/tv/show/get', middleware.index, FavoriteTvShow.index)
+route.get('/favorite/tv/show/:user_id', middleware.show, FavoriteTvShow.show)
+// route.put('/favorite/tv/show/:_id', middleware.update, FavoriteTvShow.update)
+route.delete('/favorite/tv/show/:_id', middleware.destroy, FavoriteTvShow.destroy)
  
 module.exports = route;
